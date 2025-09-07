feae087e81963e2b6103560f708e1501e1f66cb0
Madhu <114853422+MadhuSuniL@users.noreply.github.com>
2025-09-07 14:19:12 +0530

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,12 +1,11 @@
 import os
-import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from vr_conv_app.urls import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django_asgi_app = get_asgi_application()os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -16,3 +15,4 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
