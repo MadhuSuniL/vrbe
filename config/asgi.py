7bf49ba0a91e5d5be71d133c7f2a6b1cc57bca8c
Madhu <114853422+MadhuSuniL@users.noreply.github.com>
2025-09-07 14:19:50 +0530

    feat: initial commit with complete Django backend (ASGI + Celery + Redis + JWT)

--- config/asgi.py
+++ config/asgi.py
@@ -5,7 +5,7 @@ from django.core.asgi import get_asgi_application
 from vr_conv_app.urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django_asgi_app = get_asgi_application()os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -15,4 +15,5 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
 
